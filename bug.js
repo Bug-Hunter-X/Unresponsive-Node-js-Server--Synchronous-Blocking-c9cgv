867e444525100a670976046294dab1157a5cba34
BugHunter-X <askzenai@gmail.com>
2025-01-25 13:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Simulate a long-running task
+  let count = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    count += i; 
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
